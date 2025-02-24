786b50873c5b10e5d8e9fcce29129e59e3b3c964
Arun <arun@milespeak.com>
2025-02-24 18:16:01 +0530

    Updated commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
-// import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const app = createApp(App)
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/Home.vue'
-import Project from '../components/Projects.vue'
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +8,7 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Home,
-    },
-    {
-      path: '/projects',
-      name: 'project',
-      component: Project,
-    },
+    }
   
   ],
 })
